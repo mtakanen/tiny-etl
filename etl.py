5051c1128ef05e526259973843c91d3ab99798b8
Takanen Marko <mtakanen@gmail.com>
2019-03-08 10:54:35 +0200

    Add unit test for etl

--- etl.py
+++ etl.py
@@ -1,11 +1,13 @@
 import argparse
 from datetime import datetime
 import logging
+import sys
 import os
 
 from extract import Extract
 import transform
 from transform import Transform
+import load
 from load import Load
 
 logger = logging.getLogger(__name__)
@@ -13,28 +15,29 @@ logger = logging.getLogger(__name__)
 SUPPORTED_GAMES=['hb', 'wwc']
 DATA_DIR='data'
 
-def main():
-    game, extract_date = parse_args()
+def main(args):
+    game, extract_date = parse_args(args)
     etl(game, extract_date)
 
-def etl(game, extract_date):
+def etl(game, extract_date, data_dir=DATA_DIR, db=load.DB_FILENAME):
     logger.info('Start ETL for game {0}'.format(game))
     load_date = datetime.today()
 
-    data_dir = os.path.join(DATA_DIR, game)
+    data_dir = os.path.join(data_dir, game)
+    
     extract_data = Extract.extract(data_dir, extract_date)
     if game == 'hb':
         trans_fun = Transform.hb_transform
     elif game == 'wwc':
         trans_fun = Transform.wwc_transform
     transform_data = Transform.transform(extract_data, trans_fun, extract_date, load_date)
-    Load.load(transform_data, game)
+    Load.load(transform_data, game, db=db)
 
-def parse_args():
+def parse_args(args):
     parser = argparse.ArgumentParser(description='Game account data ETL')
     parser.add_argument('game', help='Name of the game')
     parser.add_argument('date', help='Data extract date in format YYYY-MM-DD')
-    args = parser.parse_args()
+    args = parser.parse_args(args)
 
     game = args.game
     if game not in SUPPORTED_GAMES:
@@ -50,4 +53,4 @@ def parse_args():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    main()
+    main(sys.argv[1:])
---
+++ test/test_etl.py
@@ -0,0 +1,62 @@
+import unittest
+import logging
+from datetime import datetime
+import sqlite3
+import os
+
+import etl
+import transform
+
+logging.disable(logging.CRITICAL)
+
+TEST_DB = 'test/test.db'
+TEST_DIR = 'test'
+
+
+class TestEtl(unittest.TestCase):
+
+    @classmethod
+    def tearDown(cls):
+        try:
+            os.remove(TEST_DB)
+        except FileNotFoundError:
+            pass
+    
+    def test_parse_args(self):
+        extract_date_str = '2019-03-01'
+        args = etl.parse_args(['hb', extract_date_str])
+        extract_date = datetime.strptime(extract_date_str, transform.EXTRACT_DATE_FORMAT).date()
+        assert args[0] == 'hb' and args[1] == extract_date
+
+    def test_parse_args_raises(self):
+        extract_date_str = '2019-03-01'
+        with self.assertRaises(SystemExit):
+            etl.parse_args(['foo', extract_date_str])
+
+    def test_parse_args_raises2(self):
+        extract_date_str = '20190301'
+        with self.assertRaises(SystemExit):
+            etl.parse_args(['hb', extract_date_str])
+
+    @classmethod
+    def test_etl_hb(cls):
+        extract_date = datetime.strptime('2019-03-02', transform.EXTRACT_DATE_FORMAT).date()
+        data_dir = os.path.join(TEST_DIR, etl.DATA_DIR)
+        etl.etl('hb', extract_date, data_dir=data_dir, db=TEST_DB)
+
+        conn = sqlite3.connect(TEST_DB)
+        c = conn.cursor()   
+        c.execute('SELECT count(*) FROM hb_accounts')
+        assert c.fetchone()[0] == 10
+
+    @classmethod
+    def test_etl_wwc(cls):
+        extract_date = datetime.strptime('2019-03-02', transform.EXTRACT_DATE_FORMAT).date()
+        data_dir = os.path.join(TEST_DIR, etl.DATA_DIR)
+        etl.etl('wwc', extract_date, data_dir=data_dir, db=TEST_DB)
+
+        conn = sqlite3.connect(TEST_DB)
+        c = conn.cursor()   
+        c.execute('SELECT count(*) FROM wwc_accounts')
+        assert c.fetchone()[0] == 5
+
